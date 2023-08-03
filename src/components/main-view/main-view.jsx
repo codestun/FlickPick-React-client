62520d52390e4d2ff48a7a51f64f75ae01fa527f
codestun <doerthy.k@gmail.com>
2023-08-03 21:23:39 +0200

    Refactor to handle selected movie and display similar movies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,33 +3,37 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
+  // State to hold the list of movies
   const [movies, setMovies] = useState([]);
 
+  // State to keep track of the selected movie
+  const [selected, setSelected] = useState(null);
+
+  // Fetch movies data from the API
   useEffect(() => {
     fetch("https://flickpick-1911bf3985c5.herokuapp.com/movies")
       .then((response) => response.json())
       .then((data) => {
-        const moviesFromApi = data.map((movie) => {
-          return {
-            id: movie._id,
-            Title: movie.Title,
-            ImagePath: movie.ImagePath,
-            Year: movie.Year,
-            Description: movie.Description,
-            Genre: {
-              Name: movie.Genre.Name,
-              Description: movie.Genre.Description,
-            },
-            Director: {
-              Name: movie.Director.Name,
-              Bio: movie.Director.Bio,
-              BirthYear: movie.Director.BirthYear,
-              DeathYear: movie.Director.DeathYear,
-              Movies: movie.Director.Movies,
-            },
-          };
-        });
-
+        // Map the API data into the required format
+        const moviesFromApi = data.map((movie) => ({
+          id: movie._id,
+          Title: movie.Title,
+          ImagePath: movie.ImagePath,
+          Year: movie.Year,
+          Description: movie.Description,
+          Genre: {
+            Name: movie.Genre.Name,
+            Description: movie.Genre.Description,
+          },
+          Director: {
+            Name: movie.Director.Name,
+            Bio: movie.Director.Bio,
+            BirthYear: movie.Director.BirthYear,
+            DeathYear: movie.Director.DeathYear,
+            Movies: movie.Director.Movies,
+          },
+        }));
+        // Set the movies state with the data from the API
         setMovies(moviesFromApi);
       })
       .catch((error) => {
@@ -37,11 +41,49 @@ export const MainView = () => {
       });
   }, []);
 
-  const [selectedMovie, setSelectedMovie] = useState(null);
+  // Function to handle the "Back" button click and reset the selected movie state
+  const onBackClick = () => {
+    setSelected(null);
+  };
 
-  if (selectedMovie) {
+  // If a movie is selected, show its details and similar movies
+  if (selected) {
+    const similarMovies = movies.filter(
+      (movie) =>
+        movie.Genre.Name === selected.Genre.Name && movie.id !== selected.id
+    );
+
     return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      <div>
+        {/* Show the details of the selected movie */}
+        <MovieView movie={selected} onBackClick={onBackClick} />
+
+        {/* Show the "Similar Movies" section if there are similar movies */}
+        {similarMovies.length > 0 && (
+          <div>
+            <h2>Similar Movies</h2>
+            <div>
+              {/* Loop through and display similar movies */}
+              {similarMovies.map((movie) => (
+                <MovieCard
+                  key={movie.id}
+                  movie={movie}
+                  onClick={() => setSelected(movie)}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {/* Show a message when no similar movies are found */}
+        {similarMovies.length === 0 && <p>No similar movies found.</p>}
+        <br />
+
+        {/* Show the "Back" button */}
+        <div>
+          <button onClick={onBackClick}>Back</button>
+        </div>
+      </div>
     );
   }
 
@@ -49,17 +91,17 @@ export const MainView = () => {
     return <div>The list is empty!</div>;
   }
 
+  // If no movie is selected, show the list of all movies
   return (
     <div>
+      {/* Loop through and display all movies */}
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
           movie={movie}
-          onClick={() => {
-            setSelectedMovie(movie);
-          }}
+          onClick={() => setSelected(movie)}
         />
       ))}
     </div>
   );
-}
+};
