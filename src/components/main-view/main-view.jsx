98fcbe9e1ec11ec1a241d9fde9e01199d4ceedfd
codestun <doerthy.k@gmail.com>
2023-08-03 15:23:24 +0200

    Populate movies state with fetched data from my flickpick API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,9 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://flickpick-1911bf3985c5.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((movie) => {
+          return {
+            id: movie._id,
+            Title: movie.Title,
+            ImagePath: movie.ImagePath,
+            Year: movie.Year,
+            Description: movie.Description,
+            Genre: {
+              Name: movie.Genre.Name,
+              Description: movie.Genre.Description,
+            },
+            Director: {
+              Name: movie.Director.Name,
+              Bio: movie.Director.Bio,
+              BirthYear: movie.Director.BirthYear,
+              DeathYear: movie.Director.DeathYear,
+              Movies: movie.Director.Movies,
+            },
+          };
+        });
+
+        setMovies(moviesFromApi);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
