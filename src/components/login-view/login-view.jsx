21f46668341e3535db35853d4858fe0b454c3c60
codestun <doerthy.k@gmail.com>
2023-08-09 11:27:34 +0200

    Update component to handle form submission and API interaction

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,13 +1,57 @@
-export const LoginView = () => {
+import { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    // this prevents the default behavior of the form which is to reload the entire page
+    event.preventDefault();
+
+    const data = {
+      Name: name,
+      Password: password
+    };
+
+    fetch("https://flickpick-1911bf3985c5.herokuapp.com/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+      .catch((e) => {
+        alert("Something went wrong");
+      });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
       </label>
       <label>
         Password:
-        <input type="password" />
+        <input type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
       </label>
       <button type="submit">Submit</button>
     </form>
