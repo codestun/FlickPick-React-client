91cad25596ea8c4a8ef87c73f25c1762d7ff39e2
codestun <doerthy.k@gmail.com>
2023-09-02 22:50:27 +0200

    Switched from axios to fetch, refined error handling, optimized movie display logic

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -3,14 +3,32 @@ import { Link } from "react-router-dom";
 import { Button, Card, Col, Figure, Row } from "react-bootstrap";
 import "./profile-view.scss";
 
-function FavoriteMovies({ favoriteMovieList }) {
-  const removeFav = id => {
+function FavoriteMovies({ favoriteMovieList, movies }) {
+  console.log("User's favorite movie IDs:", favoriteMovieList.map(movie => movie._id));
+
+  const removeFav = (_id) => {
     let token = localStorage.getItem("token");
-    let url = `https://flickpick-1911bf3985c5.herokuapp.com/users/${localStorage.getItem("user")}/movies/${id}`;
-    axios.delete(url, {
-      headers: { Authorization: `Bearer ${token}` },
+    let user = JSON.parse(localStorage.getItem("user"));
+
+    let url = `https://flickpick-1911bf3985c5.herokuapp.com/users/${user.Name}/movies/${_id}`;
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      }
     })
-  }
+      .then(response => {
+        if (!response.ok) throw new Error(response.statusText);
+        return response.json();
+      })
+      .then(data => {
+        console.log("Movie removed from favorites:", data);
+      })
+      .catch(error => {
+        console.error("Error removing movie from favorites:", error);
+      });
+  };
 
   return (
     <Card>
@@ -21,31 +39,28 @@ function FavoriteMovies({ favoriteMovieList }) {
           </Col>
         </Row>
         <Row>
-          {favoriteMovieList.map((ImagePath, Title, _id) => {
+          {favoriteMovieList.map(({ ImagePath, Title, _id }) => {
+            if (!favoriteMovieList.length) {
+              return <div>You have no favorite movies.</div>;
+            }
             return (
               <Col xs={12} md={6} lg={3} key={_id} className="fav-movie">
                 <Figure>
                   <Link to={`/movies/${_id}`}>
-                    <Figure.Image
-                      src={ImagePath}
-                      alt={Title}
-                    />
-                    <Figure.Caption>
-                      Title
-                    </Figure.Caption>
+                    <Figure.Image src={ImagePath} alt={Title} />
+                    <Figure.Caption>{Title}</Figure.Caption>
                   </Link>
                 </Figure>
-                <Button variant="secondary" onClick={() => removeFav(_id)}>
+                <Button variant="danger" onClick={() => removeFav(_id)}>
                   Remove
                 </Button>
               </Col>
-            )
-          })
-          }
+            );
+          })}
         </Row>
       </Card.Body>
     </Card>
-  )
+  );
 }
 
-export default FavoriteMovies
+export default FavoriteMovies;
